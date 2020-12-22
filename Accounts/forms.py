fc818943c0583034cdd64ff919c984c08ba54955
Artis Johnson <artisj3@gmail.com>
2020-12-22 18:43:06 +0000

    Fixed messages being sent to the form

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -4,8 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+
 class CreateUserForm(UserCreationForm):
-  class Meta:
-    model = User
-    # order of the list will show in the display
-    fields = ['username','email','password1','password2']
+    class Meta:
+        model = User
+        # order of the list will show in the display
+        fields = ['username', 'email', 'password1', 'password2']
--- Accounts/views.py
+++ Accounts/views.py
@@ -8,17 +8,19 @@ from django.contrib import messages
 
 # Create your views here.
 def registerPage(request):
-  form = CreateUserForm()
-  if request.method == 'POST':
-    #gets post data
-    form = CreateUserForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      #gets data from the form, to display when account is created
-      username = form.cleaned_data.get('username')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        #gets post data
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            #gets data from the form, to display when account is created
+            username = form.cleaned_data.get('username')
 
-      Customer.objects.create(user=user,name=user.username)
-      messages.success(request, 'Account has been created for ' + username)
-      return HttpResponse('Sucess')
+            Customer.objects.create(user=user, name=user.username)
+            #send message to form
+            messages.success(request,
+                             'Account has been created for ' + username)
+            #return HttpResponse('Sucess')
 
-  return render(request,'Accounts/register.html', {'form': form})
+    return render(request, 'Accounts/register.html', {'form': form})
